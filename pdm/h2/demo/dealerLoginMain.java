45519cca900092af51ac0aa301e5c815889d0546
Andrew Necoechea <ajn2045@rit.edu>
2018-04-14 13:51:29 -0400

    Updated version of make and gender

--- pdm/h2/demo/dealerLoginMain.java
+++ pdm/h2/demo/dealerLoginMain.java
@@ -1146,23 +1146,23 @@ public class dealerLoginMain {
 
     public static void makeAndGender(H2DemoMain demo, Dealer currentDealer) {
 
-
-//        SELECT VEHICLES.MAKE, VEHICLES.VIN, CUSTOMER.GENDER, SUM(TOTAL)
-//        FROM VEHICLES
-//        INNER JOIN VEHICLES_SOLD_TO_CUSTOMER ON VEHICLES_SOLD_TO_CUSTOMER.VIN = VEHICLES.VIN
-//        INNER JOIN CUSTOMER_SALE_TABLE ON CUSTOMER_SALE_TABLE.SALE_ID = VEHICLES_SOLD_TO_CUSTOMER.SALE_ID
-//        INNER JOIN CUSTOMER ON CUSTOMER.SSN = CUSTOMER_SALE_TABLE.SSN
-//        GROUP BY VEHICLES.MAKE, VEHICLES.VIN, CUSTOMER.GENDER
-//        ORDER BY MAKE
+ //       SELECT VEHICLES.MAKE, VEHICLES.VIN, CUSTOMER.GENDER, SUM(CUSTOMER_SALE_TABLE.TOTAL)
+ //       FROM VEHICLES
+ //       INNER JOIN VEHICLES_SOLD_TO_CUSTOMER ON VEHICLES_SOLD_TO_CUSTOMER.VIN = VEHICLES.VIN
+ //       INNER JOIN CUSTOMER_SALE_TABLE ON CUSTOMER_SALE_TABLE.SALE_ID = VEHICLES_SOLD_TO_CUSTOMER.SALE_ID
+  //      INNER JOIN CUSTOMER ON CUSTOMER.SSN = CUSTOMER_SALE_TABLE.SSN
+  //      WHERE CUSTOMER_SALE_TABLE.DEALER_ID = 'jctgn'
+  //      GROUP BY VEHICLES.MAKE, VEHICLES.VIN, CUSTOMER.GENDER
+  //      ORDER BY MAKE
 
 
         try {
 
-            String queryMakeAndGender = "SELECT vehicles.Make, vehicles.VIN, customer.Gender, SUM(TOTAL) FROM vehicles"
+            String queryMakeAndGender = "SELECT vehicles.Make, vehicles.VIN, customer.Gender, SUM(customer_sale_table.Total) FROM vehicles"
                     + " INNER JOIN vehicles_sold_to_customer ON vehicles_sold_to_customer.VIN = vehicles.VIN"
                     + " INNER JOIN customer_sale_table ON customer_sale_table.Sale_ID = vehicles_sold_to_customer.Sale_ID"
-                    + " INNER JOIN customer ON customer.SSN = customer_sale_table.SSN"
-                    + " GROUP BY vehicles.Make, vehicles.VIN, customer.Gender"
+                    + " INNER JOIN customer ON customer.SSN = customer_sale_table.SSN WHERE customer_sale_table.Dealer_ID =\'" + currentDealer.getDealer_ID()
+                    + "\' GROUP BY vehicles.Make, vehicles.VIN, customer.Gender"
                     + " ORDER BY Make;";
             Statement stmt = demo.getConnection().createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             ResultSet result = stmt.executeQuery(queryMakeAndGender);
